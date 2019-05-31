a8c519704fa868139ebb48ccd4db0e1fc88929f9
Franklin Chieze <chieze.franklin@gmail.com>
2019-05-31 16:20:32 +0100

    get testing up and running

--- src/store.js
+++ src/store.js
@@ -3,13 +3,23 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const initialState = [];
+const initialState = {};
 
 const middleware = [thunk];
 
+const devtools = process.env.NODE_ENV === 'test'
+  ? x => x /* eslint-disable no-underscore-dangle */
+  : window.__REDUX_DEVTOOLS_EXTENSION__
+      && window.__REDUX_DEVTOOLS_EXTENSION__();
+/* eslint-enable no-underscore-dangle */
+
 const store = createStore(rootReducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  devtools
 ));
-
+// const store = compose(
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )(createStore)(rootReducer);
+console.log(store)
 export default store;
